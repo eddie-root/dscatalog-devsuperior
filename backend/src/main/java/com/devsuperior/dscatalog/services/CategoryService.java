8ad59f50f1c1f4788db2c6b7c3cfa35354080b9c
Admilson Ferreira <ferreiraad3@gmail.com>
2022-12-29 16:34:43 -0300

    H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+
+	@GetMapping
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	
+}
